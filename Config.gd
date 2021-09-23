06e9ce33ea194f3f82bee5db9f48ade1fa14c949
C.H <oh6for@gmail.com>
2021-09-23 19:32:39 +0300

    -Add script for configuration a.ka options dialog.

--- Config.gd
+++ Config.gd
@@ -4,3 +4,10 @@ extends WindowDialog
 
 func _ready():
 	pass
+
+
+func _on_Maps_Button_pressed():
+	get_node(".").hide()
+	get_node("../Maps_Configuration_WindowDialog").popup_centered()
+	
+	pass # Replace with function body.
