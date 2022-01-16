80f13b1372d09bab9a810e5a36b7cecdaa39922a
C.H <oh6for@gmail.com>
2022-01-16 18:06:40 +0200

    -Add very early script, for handling icons of flags for country selection. "Will most likely be deleted, if and when WMS / WFS / OGC is introduced"

---
+++ Scenes/Main_menu/Enviroment_Settings/Scripts/Enviroment_Settings.gd
@@ -0,0 +1,9 @@
+extends WindowDialog
+
+var flags_path = "res://Scenes/Main_menu/Enviroment_Settings/Icons/Country_Flags/country.json"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
