ba61761ebc62c4e6e39df1a189288201272a15f8
C.H <oh6for@gmail.com>
2022-08-19 13:08:06 +0300

    -Add temporary fix for theme song audio player not stopping.

--- Scenes/Main_menu/Scripts/Main_Menu.gd
+++ Scenes/Main_menu/Scripts/Main_Menu.gd
@@ -35,8 +35,10 @@ func play_theme_music():
 	audio_player.set_stream(track)
 	#Connect finished signal to finished function.
 	audio_player.connect("finished",self,"AudioStreamPlayer_Finished")
-	#play theme song.
-	audio_player.play()
+	#Temporary Fix.
+	if ($".").is_visible():
+		#play theme song.
+		audio_player.play()
 
 #Function for animating the Open-Crops header. "Slide in"
 func animate_header():
@@ -53,81 +55,7 @@ func animate_header():
 	#Play the current animation.
 	header_anim_player.play()
 
-#Reset labels in prompt window.
-func _On_Prompt_User_WindowDialog_Hide():
-	reset_user_prompt_window("","","","")
 
-#Unsure if bbcode should be used for reset as well ?.
-func reset_user_prompt_window(var window_header,var header_label,var footer_label,var user_action):
-	#Re-Set The Window Header Label.
-	get_node("Prompt_User_WindowDialog").window_title = window_header
-	#Re-Set The Header Label.
-	get_node("Prompt_User_WindowDialog/Prompt_User_Window_Header_Label").text = header_label
-	#Re-Set The Footer Label.
-	get_node("Prompt_User_WindowDialog/Prompt_User_Window_Footer_Label").text = footer_label
-	#Re-Set Hoax Symbol.
-	get_node("Prompt_User_WindowDialog/Visibility_Control_Hox_Symbol").hide()
-	#Stop Supercool Hoax Symbol Animation & Reset.
-	get_node("Prompt_User_WindowDialog/Hox_Symbol_Blink_Animation").stop(true)
-
-#Draw User Prompt Window.
-func display_user_prompt_window(var window_header,var header_label,var footer_label,var user_action):
-	
-	#Show User Prompt Window.
-	get_node("Prompt_User_WindowDialog").show()
-	
-	#Set The Window Header Label.
-	get_node("Prompt_User_WindowDialog").window_title = window_header
-	#Set The Header Label.
-	get_node("Prompt_User_WindowDialog/Prompt_User_Window_Header_Label").bbcode_text = "[center]"+header_label+"[/center]"
-	#Set The Footer Label.
-	get_node("Prompt_User_WindowDialog/Prompt_User_Window_Footer_Label").bbcode_text = "[center]"+footer_label+"[/center]"
-	
-	if window_header == "Error":
-		#Display Hox Symbol.
-		get_node("Prompt_User_WindowDialog/Visibility_Control_Hox_Symbol").show()
-		#Also
-		#Select & Start Our Supercool Hox Animation.
-		get_node("Prompt_User_WindowDialog/Hox_Symbol_Blink_Animation").play("Hox_Symbol_Blink") 
-	elif window_header == "Warning":
-		#Display Hox Symbol.
-		get_node("Prompt_User_WindowDialog/Visibility_Control_Hox_Symbol").show()
-		#Also
-		#Select & Start Our Supercool Hox Animation.
-		get_node("Prompt_User_WindowDialog/Hox_Symbol_Blink_Animation").play("Hox_Symbol_Blink")
-	
-	if user_action == "Select":
-		get_node("Prompt_User_WindowDialog/Visibility_Control_Selection_Buttons").show()
-
-
-func check_asset_essentials():
-	#Check If We Have Assets Folder Present.
-	#If Not, Lets Try To Do Something About it.
-	
-	
-	#Do We Have The New Assets Library?
-	var check_assets_dir = Directory.new().dir_exists("res://Assets/")
-	if !check_assets_dir:
-		#If Not, Notify User.
-		display_user_prompt_window("Error","Asset Library Missing","Add Library & Download Content?","select")
-		#Do HttpRequest In Future.
-		
-	if check_assets_dir:
-		#We Got The Assets Folder.
-		#Check For Maps Folder As Next.
-		var check_map_dir = Directory.new().dir_exists("res://Assets/Terrain")
-		if !check_map_dir:
-			display_user_prompt_window("Warning","Maps Folder Missing","Add Library & Re-Download Content?","Select")
-		
-		#Launch Game Enviroment & Handle Essentials For That.
-		else:
-			#Hide Main Menu
-			get_node(".").hide()
-			#Sadly, Stop Our Superawesome Intro Song.
-			audio_player.stop()
-			#Add Game Enviroment Scene.
-			add_child(game_enviroment)
-			#Note to self, remember to free audiostreamplayer.
 
 func launch_enviroment():
 	#Hide Main Menu
